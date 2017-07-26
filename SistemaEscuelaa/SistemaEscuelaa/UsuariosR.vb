0984a2a4b6eccfe40d1aa6038489431b093f6a01
ozblancas <ozblancas@gmail.com>
2017-07-26 00:56:02 -0500

    Form UsuariosR
    FORM, donde el reporte es generado y mandado llamar

---
+++ SistemaEscuelaa/SistemaEscuelaa/UsuariosR.vb
@@ -0,0 +1,20 @@
+﻿Imports CrystalDecisions.CrystalReports.Engine
+Imports CrystalDecisions.Shared
+Public Class UsuariosR
+
+    Private Sub CrystalReportViewer1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CrystalReportViewer1.Load
+
+    End Sub
+
+    Private Sub UsuariosR_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub j_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles j.Click
+        Dim obj As New CrystalReport1
+
+
+        CrystalReportViewer1.ReportSource = obj
+        CrystalReportViewer1.Refresh()
+    End Sub
+End Class
