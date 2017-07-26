29c6e2d5c11b37803f875ff09069df315985ff32
ozblancas <ozblancas@gmail.com>
2017-07-26 00:51:33 -0500

    vista principal de reportes
    contenido opción de reportes

---
+++ SistemaEscuelaa/SistemaEscuelaa/Reportes.vb
@@ -0,0 +1,7 @@
+﻿Public Class Reportes
+
+    Private Sub GenerarReporteToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GenerarReporteToolStripMenuItem.Click
+        UsuariosR.Show()
+
+    End Sub
+End Class
