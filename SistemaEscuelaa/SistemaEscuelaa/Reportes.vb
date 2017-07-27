4a62ae5d9d4655f2297e2569405d6feb3051c0b5
ozblancas <ozblancas@gmail.com>
2017-07-27 01:21:43 -0500

    vista principal reportes actualizacion
    actualización de vinculos, agregacion de reportes

--- SistemaEscuelaa/SistemaEscuelaa/Reportes.vb
+++ SistemaEscuelaa/SistemaEscuelaa/Reportes.vb
@@ -4,4 +4,14 @@
         UsuariosR.Show()
 
     End Sub
+
+    Private Sub GenerarReporteToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GenerarReporteToolStripMenuItem1.Click
+        AlumnosGC.Show()
+
+    End Sub
+
+    Private Sub GenerarReporteToolStripMenuItem2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GenerarReporteToolStripMenuItem2.Click
+        ProfesoresGC.Show()
+
+    End Sub
 End Class
