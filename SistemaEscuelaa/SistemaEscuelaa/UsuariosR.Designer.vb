5aa932cb0faf5e4142361a33a0d8500a0a77ff55
ozblancas <ozblancas@gmail.com>
2017-07-27 02:47:39 -0500

    vista usuarios

---
+++ SistemaEscuelaa/SistemaEscuelaa/UsuariosR.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UsuariosR
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CrystalReportViewer1 = New CrystalDecisions.Windows.Forms.CrystalReportViewer
+        Me.j = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'CrystalReportViewer1
+        '
+        Me.CrystalReportViewer1.ActiveViewIndex = -1
+        Me.CrystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.CrystalReportViewer1.DisplayGroupTree = False
+        Me.CrystalReportViewer1.Location = New System.Drawing.Point(12, 51)
+        Me.CrystalReportViewer1.Name = "CrystalReportViewer1"
+        Me.CrystalReportViewer1.SelectionFormula = ""
+        Me.CrystalReportViewer1.Size = New System.Drawing.Size(714, 279)
+        Me.CrystalReportViewer1.TabIndex = 0
+        Me.CrystalReportViewer1.ViewTimeSelectionFormula = ""
+        '
+        'j
+        '
+        Me.j.Location = New System.Drawing.Point(12, 12)
+        Me.j.Name = "j"
+        Me.j.Size = New System.Drawing.Size(164, 23)
+        Me.j.TabIndex = 1
+        Me.j.Text = "Generar reporte de usuarios "
+        Me.j.UseVisualStyleBackColor = True
+        '
+        'UsuariosR
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(738, 342)
+        Me.Controls.Add(Me.j)
+        Me.Controls.Add(Me.CrystalReportViewer1)
+        Me.Name = "UsuariosR"
+        Me.Text = "UsuariosR"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents CrystalReportViewer1 As CrystalDecisions.Windows.Forms.CrystalReportViewer
+    Friend WithEvents j As System.Windows.Forms.Button
+End Class
